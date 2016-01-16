5b5b5da50687d522bf2b6e8024d30ec7e6b1c538
Chris Connell <chris.g.connell@gmail.com>
2016-01-16 12:39:44 -0600

    video corrected

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,57 +2,27 @@ class PagesController < ApplicationController
 
   def get_example
     @message = params[:message]
-    @messaeg2 = params[:message2]
+    @message2 = params[:message2]
   end
 
   def get_name
-    # @name = params[:name].upcase
-    # if @anme.starts_with?("A")
-    #   @message = "That name starts with an 'A'"
+    @name = params[:name].upcase
+    if @name.starts_with?("A")
+      @message = "That starts with and 'A'"
+    end
   end
 
   def number_game
-    guess = params[:purple_hippo].to_i
+    guess = params[:number].to_i
     correct_answer = 42
 
-    if guess > correct_answer
-      @message == "you got it right!!!!"
+    if guess == correct_answer
+      @message = "you got it right"
     elsif guess < correct_answer
-      @message = "you guess too low."
-    else
-      @message = "you guessed too high."    
+      @message = "you guessed too low"
+    else guess
+      @message = "you guess too high"
+    end
   end
-
-  def url_example
-    @message = params[:wildcard]
-  end
-
-  def get_form
-
-  end
-
-  def submit_form
-    @message = params[:message]
-  end
-
-  def number_form
-    
-  end
-
-  def number_results
-    @guess.to_i
-    correct_answer = 42
-
-    if @guess > correct_answer
-      @message== "you got it right!!!!"
-    elsif  @guess < correct_answer
-      @gmessage= "you guess too low."
-    else
-      @message = "you guessed too high."    
-  end
-  end
-end
-
-
 
 end
